bfee5359aad5fabe562906f3fa5a6700320bfc0a
Choi Jiwon <winning1993@naver.com>
2020-06-16 07:34:15 +0900

    Enable paddle smooth moving

--- script.js
+++ script.js
@@ -16,15 +16,62 @@ function initBlocks(){
 }
 
 function controlCart(e){
+    console.log(e)
+
     const pressedKey = e.key;
-    console.log(pressedKey)
-    console.log(positionIndex)
-    if(pressedKey === "ArrowRight" && positionIndex > -44){
-        cart.style.right = `${positionIndex-=4}%`;
-    }
-    else if(pressedKey === "ArrowLeft" && positionIndex < 44){
-        cart.style.right = `${positionIndex+=4}%`;
-    }
+        if(pressedKey === "ArrowRight"){
+            if(e.repeat === true){
+                const moveRight = setInterval(() => {
+                    if(positionIndex > -44){
+                        cart.style.right = `${positionIndex-=0.1}%`;
+                    }
+                },5);
+                setTimeout(()=>{
+                    clearInterval(moveRight);
+                },100) // 0.1초 동안 0.005 초마다 -0.1%씩 움직임
+            }
+            else{
+                const moveRight = setInterval(() => {
+                    if(positionIndex > -44){
+                        cart.style.right = `${positionIndex-=0.2}%`;
+                        window.addEventListener("keyup", ()=>{
+                            clearInterval(moveRight);
+                        });
+                    }
+                },5);
+                setTimeout(()=>{
+                    clearInterval(moveRight);
+                },550) // 0.5초 동안 0.005 초마다 -0.1%씩 움직임
+            }
+        }
+        else if(pressedKey === "ArrowLeft"){
+            if(e.repeat === true){
+                const moveLeft = setInterval(() => {
+                    if(positionIndex < 43){
+                        cart.style.right = `${positionIndex+=0.1}%`;
+                    }
+                },5);
+                setTimeout(()=>{
+                    clearInterval(moveLeft);
+                },100)
+            }
+            else{
+                const moveLeft = setInterval(() => {
+                    if(positionIndex < 43){
+                        cart.style.right = `${positionIndex+=0.2}%`;
+                        window.addEventListener("keyup", ()=>{
+                            clearInterval(moveLeft);
+                        });
+                    }
+                },5);
+                setTimeout(()=>{
+                    clearInterval(moveLeft);
+                },550)
+            }
+        }
+    
+    
+
 }
 
 function openModal(){
