c0a7f031df08c1652592a6e45f3affb5bbdf3ec3
Choi Jiwon <winning1993@naver.com>
2020-06-16 19:44:51 +0900

    Create ball and make it bouncing

--- script.js
+++ script.js
@@ -3,8 +3,43 @@ const modalBtn = document.getElementById("modalBtn");
 const closeModalBtn = document.getElementById("closeModalBtn");
 const modal = document.getElementById("modal");
 const blockArea = document.getElementById("blockArea");
+const ball = document.getElementById("ball");
+const mainScreen = document.getElementById("mainScreen");
+const ctx = mainScreen.getContext("2d");
 
+
+console.log(ctx)
 let positionIndex = 0;
+let ballDirectionDeg = 0;
+
+
+let x = mainScreen.width/2;
+let y = mainScreen.height-30;
+let dx = 2;
+let dy = -2;
+let ballRadius = 10;
+
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, mainScreen.width, mainScreen.height);
+    drawBall();
+    x += dx;
+    y += dy;
+    if(x + dx > mainScreen.width || x + dx < 0) {
+        dx = -dx;
+    }
+    
+    if(y + dy > mainScreen.height || y + dy < 0) {
+        dy = -dy;
+    }
+}
 
 function initBlocks(){
     for(let i=0; i<40 ; i++){
@@ -41,7 +76,7 @@ function controlCart(e){
                 },5);
                 setTimeout(()=>{
                     clearInterval(moveRight);
-                },550) // 0.5초 동안 0.005 초마다 -0.1%씩 움직임
+                },550) // 키를 꾹 누르고 있을 떄 repeat === true가 활성화되기 까지 길리는 시간 (약 0.5~0.6초)
             }
         }
         else if(pressedKey === "ArrowLeft"){
@@ -53,7 +88,7 @@ function controlCart(e){
                 },5);
                 setTimeout(()=>{
                     clearInterval(moveLeft);
-                },100)
+                },100) // 0.1초 동안 0.005 초마다 -0.1%씩 움직임
             }
             else{
                 const moveLeft = setInterval(() => {
@@ -66,12 +101,9 @@ function controlCart(e){
                 },5);
                 setTimeout(()=>{
                     clearInterval(moveLeft);
-                },550)
+                },550) // 키를 꾹 누르고 있을 떄 repeat === true가 활성화되기 까지 길리는 시간 (약 0.5~0.6초)
             }
         }
-    
-    
-
 }
 
 function openModal(){
@@ -82,11 +114,17 @@ function closeModal(){
     modal.style.animation = "modalClose 1s forwards"; 
 }
 
+function ballPhysics(){
+    console.log(ball.offsetWidth)
+}
+
 function init(){
     window.addEventListener("keydown", controlCart);
     modalBtn.addEventListener("click", openModal);
     closeModalBtn.addEventListener("click", closeModal);
     initBlocks()
+    ballPhysics()
+    setInterval(draw, 10);
 }
 
 init();
